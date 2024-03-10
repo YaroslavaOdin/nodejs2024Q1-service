f1cd309f1717abc0ed59e9ce4de8fe41333ad98a
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-03-10 20:59:37 +0300

    feat: add logic for album.

---
+++ src/album/dto/update-album.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsInt, IsOptional } from 'class-validator';
+
+export class UpdateAlbumDto {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  year: number;
+
+  @IsString()
+  @IsOptional()
+  artistId: string | null;
+}
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Album } from 'src/album/interface/album.interface';
 import { Artist } from 'src/artist/interface/artist.interface';
 import { Track } from 'src/track/interface/track.interface';
 import { User } from 'src/user/interface/user.interface';
@@ -8,4 +9,5 @@ export class Database {
   public users: User[] = [];
   public artists: Artist[] = [];
   public tracks: Track[] = [];
+  public albums: Album[] = [];
 }
