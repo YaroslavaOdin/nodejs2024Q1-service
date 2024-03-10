d9077b95a489e8f6fd09605688fc5f7d257e208e
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-03-10 15:28:18 +0300

    feat: add logic for artist.

---
+++ src/artist/artist.service.ts
@@ -0,0 +1,73 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Database } from 'src/database/database';
+import { Artist } from './interface/artist.interface';
+import { v4 as uuidv4, validate } from 'uuid';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Injectable()
+export class ArtistService {
+  constructor(private database: Database) {}
+
+  findAllArtists(): Artist[] {
+    return this.database.artists;
+  }
+
+  findOneArtist(id: string): Artist {
+    if (!validate(id)) {
+      throw new HttpException('Invalid ID.', HttpStatus.BAD_REQUEST);
+    }
+    const artist = this.database.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new HttpException('Artist not found.', HttpStatus.NOT_FOUND);
+    }
+    return artist;
+  }
+
+  createArtist(artistDto: CreateArtistDto) {
+    if (!artistDto?.name || !artistDto?.grammy) {
+      throw new HttpException('Invalid artist data.', HttpStatus.BAD_REQUEST);
+    }
+
+    const newArtist: Artist = {
+      id: uuidv4(),
+      name: artistDto.name,
+      grammy: artistDto.grammy,
+    };
+
+    this.database.artists.push(newArtist);
+
+    return newArtist;
+  }
+
+  updateArtist(id: string, artistDto: UpdateArtistDto): Artist {
+    if (
+      !validate(id) ||
+      typeof artistDto?.name !== 'string' ||
+      typeof artistDto?.grammy !== 'boolean'
+    ) {
+      throw new HttpException('Invalid ID.', HttpStatus.BAD_REQUEST);
+    }
+
+    const artist = this.database.artists.find((artist) => artist.id === id);
+
+    if (!artist) {
+      throw new HttpException('Artist not found.', HttpStatus.NOT_FOUND);
+    }
+
+    artist.name = artistDto?.name ?? artist.name;
+    artist.grammy = artistDto?.grammy ?? artist.grammy;
+
+    return artist;
+  }
+
+  removeArtist(id: string): void {
+    if (!validate(id)) {
+      throw new HttpException('Invalid ID.', HttpStatus.BAD_REQUEST);
+    }
+    const removedArtist = this.findOneArtist(id);
+    this.database.artists = this.database.artists.filter(
+      (artist) => artist.id !== removedArtist.id,
+    );
+  }
+}
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Artist } from 'src/artist/interface/artist.interface';
 import { User } from 'src/user/interface/user.interface';
 
 @Injectable()
 export class Database {
   public users: User[] = [];
+  public artists: Artist[] = [];
 }
