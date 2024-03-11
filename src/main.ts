eb2d2aad5bd7cb0459bd568a617b7c7c98ff5720
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-03-11 22:41:20 +0300

    feat: add OpenAPI.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import * as yaml from 'js-yaml';
 
 dotenv.config();
 
@@ -8,6 +11,11 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf8' }),
+  ) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT, () => {
     console.log(`The server is running at the address localhost:${PORT}.`);
   });
